0d00da7742ef942caca61d739505a43bcd045224
r3ny0u <renyou14@gmail.com>
2024-09-28 17:29:06 +0800

    trying something

--- rtl/custom_axi_ip/custom_axi_ip.sv
+++ rtl/custom_axi_ip/custom_axi_ip.sv
@@ -14,6 +14,9 @@ module custom_axi_ip (
   logic [31:0] reg0;
   logic [31:0] reg1;
   logic [31:0] reg2;
+  logic [31:0] reg3 = 32'h2468;
+  logic [31:0] reg4 = 32'h369C;
+  logic [31:0] reg5 = 32'h48D0;
 
   // Write logic for registers
   always_ff @(posedge clk_i or negedge rst_ni) begin
@@ -25,19 +28,19 @@ module custom_axi_ip (
     end else begin
       // $display("reg2ip_en_in: %b", reg2ip_en_in);
       // Write to registers based on reg2ip inputs
-      if (reg2ip_en_in[0]) begin
+      if (reg2ip_en_in[0] && reg2ip_en_out[0] != 1'b1) begin
         reg0 <= reg2ip_data[0];
         reg2ip_en_out[0] <= 1'b1;
         $display("reg0: %b", reg0);
       end
 
-      if (reg2ip_en_in[1]) begin 
+      if (reg2ip_en_in[1] && !reg2ip_en_out[1] != 1'b1) begin 
         reg1 <= reg2ip_data[1];
         reg2ip_en_out[1] <= 1'b1;
         $display("reg1: %b", reg1);
       end
 
-      if (reg2ip_en_in[2]) begin
+      if (reg2ip_en_in[2] && !reg2ip_en_out[2] != 1'b1) begin
         reg2 <= reg2ip_data[2];
         reg2ip_en_out[2] <= 1'b1;
         $display("reg2: %b", reg2);
@@ -45,13 +48,15 @@ module custom_axi_ip (
     end
   end
 
+  
+
   // Read logic would populate ip2reg based on internal state
   always_ff @(posedge clk_i) begin
-    ip2reg_data[0] <= reg0;
+    ip2reg_data[0] <= reg3;
     ip2reg_en[0]   <= 1'b1;  // Set data ready flag
-    ip2reg_data[1] <= reg1;
+    ip2reg_data[1] <= reg4;
     ip2reg_en[1]   <= 1'b1;
-    ip2reg_data[2] <= reg2;
+    ip2reg_data[2] <= reg5;
     ip2reg_en[2]   <= 1'b1;
   end
 
