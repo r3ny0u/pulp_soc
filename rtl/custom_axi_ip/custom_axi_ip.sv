8b2fb447116ef09973af158d6e95c643d9f61fac
r3ny0u <renyou14@gmail.com>
2024-09-28 19:24:22 +0800

    trying

--- rtl/custom_axi_ip/custom_axi_ip.sv
+++ rtl/custom_axi_ip/custom_axi_ip.sv
@@ -35,6 +35,7 @@ module custom_axi_ip (
         reg2ip_en_out[0] <= 1'b1;
         $display("reg2ip_en_in: %h", reg2ip_en_in);
         $display("reg2in_en_out: %h", reg2ip_en_out);
+        $display("reg0_data: %h", reg2ip_data[0]);
         $display("reg0: %h", reg0);
       end
 
@@ -43,6 +44,7 @@ module custom_axi_ip (
         reg2ip_en_out[1] <= 1'b1;
         $display("reg2ip_en_in: %h", reg2ip_en_in);
         $display("reg2in_en_out: %h", reg2ip_en_out);
+        $display("reg1_data: %h", reg2ip_data[1]);
         $display("reg1: %h", reg1);
       end
 
@@ -51,25 +53,26 @@ module custom_axi_ip (
         reg2ip_en_out[2] <= 1'b1;
         $display("reg2ip_en_in: %h", reg2ip_en_in);
         $display("reg2in_en_out: %h", reg2ip_en_out);
+        $display("reg2_data: %h", reg2ip_data[2]);
         $display("reg2: %h", reg2);
       end
     end
   end
 
-  // Read logic would populate ip2reg based on internal state
-  always_ff @(posedge clk_i) begin
-    if (!(reg2ip_en_in[2:0] & 3b'111)) begin
-      ip2reg_data[0] <= reg3;
-      ip2reg_en[0]   <= 1'b1;  // Set data ready flag
-      ip2reg_data[1] <= reg4;
-      ip2reg_en[1]   <= 1'b1;
-      ip2reg_data[2] <= reg5;
-      ip2reg_en[2]   <= 1'b1;
-    end else begin
-      ip2reg_en[0] <= 1'b0;
-      ip2reg_en[1] <= 1'b0;
-      ip2reg_en[2] <= 1'b0;
-    end
-  end
+  // // Read logic would populate ip2reg based on internal state
+  // always_ff @(posedge clk_i) begin
+  //   if (!(reg2ip_en_in[2:0] & 3'b111)) begin
+  //     ip2reg_data[0] <= reg3;
+  //     ip2reg_en[0]   <= 1'b1;  // Set data ready flag
+  //     ip2reg_data[1] <= reg4;
+  //     ip2reg_en[1]   <= 1'b1;
+  //     ip2reg_data[2] <= reg5;
+  //     ip2reg_en[2]   <= 1'b1;
+  //   end else begin
+  //     ip2reg_en[0] <= 1'b0;
+  //     ip2reg_en[1] <= 1'b0;
+  //     ip2reg_en[2] <= 1'b0;
+  //   end
+  // end
 
 endmodule
