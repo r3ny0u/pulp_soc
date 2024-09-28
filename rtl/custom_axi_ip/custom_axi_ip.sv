33e140f962413e270b6d361feac2e9a7d9e427ee
r3ny0u <renyou14@gmail.com>
2024-09-28 21:47:27 +0800

    error in write data

--- rtl/custom_axi_ip/custom_axi_ip.sv
+++ rtl/custom_axi_ip/custom_axi_ip.sv
@@ -9,7 +9,7 @@ module custom_axi_ip
     input  logic [DATA_WIDTH-1:0] reg2ip_data,
     input  logic [2:0] reg2ip_en_in,
     output logic [2:0] reg2ip_en_out,
-    output logic [DATA_WIDTH-1:0] ip2reg_data,
+    output logic [DATA_WIDTH+2:0] ip2reg_data,
     output logic [2:0] ip2reg_en
 );
 
@@ -33,27 +33,27 @@ module custom_axi_ip
       // $display("reg2ip_en_in: %h", reg2ip_en_in);
       // $display("reg2ip_en_out: %h", reg2ip_en_out);
       // Write to registers based on reg2ip inputs
-      if (reg2ip_en_in[0] && !reg2ip_en_out[0]) begin
+      if (reg2ip_en_in[0]) begin
         reg0 <= reg2ip_data[95:64];
-        reg2ip_en_out[0] <= 1'b1;
+        reg2ip_en_out[0] <= 1'b0;
         $display("reg2ip_en_in: %h", reg2ip_en_in);
         $display("reg2in_en_out: %h", reg2ip_en_out);
         $display("reg0_data: %h", reg2ip_data[0]);
         $display("reg0: %h", reg0);
       end
 
-      if (reg2ip_en_in[1] && !reg2ip_en_out[1]) begin 
+      if (reg2ip_en_in[1]) begin 
         reg1 <= reg2ip_data[63:32];
-        reg2ip_en_out[1] <= 1'b1;
+        reg2ip_en_out[1] <= 1'b0;
         $display("reg2ip_en_in: %h", reg2ip_en_in);
         $display("reg2in_en_out: %h", reg2ip_en_out);
         $display("reg1_data: %h", reg2ip_data[1]);
         $display("reg1: %h", reg1);
       end
 
-      if (reg2ip_en_in[2] && !reg2ip_en_out[2]) begin
+      if (reg2ip_en_in[2]) begin
         reg2 <= reg2ip_data[31:0];
-        reg2ip_en_out[2] <= 1'b1;
+        reg2ip_en_out[2] <= 1'b0;
         $display("reg2ip_en_in: %h", reg2ip_en_in);
         $display("reg2in_en_out: %h", reg2ip_en_out);
         $display("reg2_data: %h", reg2ip_data[2]);
@@ -65,8 +65,8 @@ module custom_axi_ip
   // Read logic would populate ip2reg based on internal state
   always_ff @(posedge clk_i) begin
     if (reg2ip_en_in[2:0] == 3'b000) begin
-      ip2reg_data <= {reg3, reg4, reg5};
-      ip2reg_en <= 3'b111;
+      ip2reg_data <= {reg3, 1'b1, reg4, 1'b1, reg5, 1'b1};
+      $display("ip2reg_data: %h", {reg3, 1'b1, reg4, 1'b1, reg5, 1'b1});
     end
   end
 
