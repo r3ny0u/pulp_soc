7d7f443b80fb71187b1cd23113245b142a15bfb2
r3ny0u <renyou14@gmail.com>
2024-09-29 23:14:11 +0800

    trying

--- rtl/custom_axi_ip/custom_axi_ip.sv
+++ rtl/custom_axi_ip/custom_axi_ip.sv
@@ -1,13 +1,12 @@
-module custom_axi_ip 
-  #(
-    parameter DATA_WIDTH = 96   
-  )(
+module custom_axi_ip #(
+    parameter DATA_WIDTH = 96
+) (
     input logic clk_i,
     input logic rst_ni,
 
     // Register to Hardware interface
-    input  logic [DATA_WIDTH-1:0] reg2ip_data,
-    input  logic [2:0] reg2ip_en_in,
+    input logic [DATA_WIDTH-1:0] reg2ip_data,
+    input logic [2:0] reg2ip_en_in,
     output logic [2:0] reg2ip_en_out,
     output logic [DATA_WIDTH+2:0] ip2reg_data,
     output logic [2:0] ip2reg_en
@@ -21,51 +20,55 @@ module custom_axi_ip
   logic [31:0] reg4 = 32'h369C;
   logic [31:0] reg5 = 32'h48D0;
 
-  // Read
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      //Reset all registers
-      reg0 <= 32'h0;
-      reg1 <= 32'h0;
-      reg2 <= 32'h0;
-      reg2ip_en_out <= 3'b0;
-    end else begin
-      // Write to registers based on reg2ip inputs
-      if (reg2ip_en_in[0]) begin
-        reg0 <= reg2ip_data[95:64];
-        reg1 <= reg2ip_data[63:32];
-        reg2 <= reg2ip_data[31:0];
-        reg2ip_en_out <= {reg2ip_en_in & 3'b110, 1'b1};
-        // $display("reg2ip_en_in: %h", reg2ip_en_in);
-        // $display("reg2in_en_out: %h", reg2ip_en_out);
-        $display("reg0: %h", reg0);
-        $display("reg1: %h", reg1);
-        $display("reg2: %h", reg2);
-      end
+  // // Read
+  // always_ff @(posedge clk_i or negedge rst_ni) begin
+  //   if (!rst_ni) begin
+  //     //Reset all registers
+  //     reg0 <= 32'h0;
+  //     reg1 <= 32'h0;
+  //     reg2 <= 32'h0;
+  //     reg2ip_en_out <= 3'b0;
+  //   end else begin
+  //     // Write to registers based on reg2ip inputs
+  //     if (reg2ip_en_in[0]) begin
+  //       reg0 <= reg2ip_data[95:64];
+  //       reg1 <= reg2ip_data[63:32];
+  //       reg2 <= reg2ip_data[31:0];
+  //       reg2ip_en_out <= {reg2ip_en_in & 3'b110, 1'b1};
+  //       $display("reg0: %h", reg0);
+  //       $display("reg1: %h", reg1);
+  //       $display("reg2: %h", reg2);
+  //     end else if (reg2ip_en_in[1]) begin
+  //       reg1 <= reg2ip_data[63:32];
+  //       reg2ip_en_out <= {reg2ip_en_in & 3'b101, 1'b1};
+  //       $display("reg1: %h", reg1);
+  //     end else if (reg2ip_en_in[2]) begin
+  //       reg2 <= reg2ip_data[31:0];
+  //       reg2ip_en_out <= {reg2ip_en_in & 3'b011, 1'b1};
+  //       $display("reg2: %h", reg2);
+  //     end
+  //   end
+  // end
 
-      if (reg2ip_en_in[1]) begin 
-        reg1 <= reg2ip_data[63:32];
-        reg2ip_en_out <= {reg2ip_en_in & 3'b101, 1'b1};
-        // $display("reg2ip_en_in: %h", reg2ip_en_in);
-        // $display("reg2in_en_out: %h", reg2ip_en_out);
-        $display("reg1: %h", reg1);
-      end
+  // // Write
+  // always_ff @(posedge clk_i) begin
+  //   if (reg2ip_en_in[2:0] == 3'b000) begin
+  //     ip2reg_data <= {reg3, 1'b0, reg4, 1'b0, reg5, 1'b0};
+  //   end
+  // end
 
-      if (reg2ip_en_in[2]) begin
-        reg2 <= reg2ip_data[31:0];
-        reg2ip_en_out <= {reg2ip_en_in & 3'b011, 1'b1};
-        // $display("reg2ip_en_in: %h", reg2ip_en_in);
-        // $display("reg2in_en_out: %h", reg2ip_en_out);
-        $display("reg2: %h", reg2);
-      end
+  always_comb begin
+    if (reg2ip_en_in[0]) begin
+      reg0 = reg2ip_data[95:64];
+      reg1 = reg2ip_data[63:32];
+      reg2 = reg2ip_data[31:0];
+      reg2ip_en_out = {reg2ip_en_in & 3'b110, 1'b1};
+      $display("reg0: %h", reg0);
+      $display("reg1: %h", reg1);
+      $display("reg2: %h", reg2);
     end
   end
 
-  // Read logic would populate ip2reg based on internal state
-  always_ff @(posedge clk_i) begin
-    if (reg2ip_en_in[2:0] == 3'b000) begin
-      ip2reg_data <= {reg3, 1'b0, reg4, 1'b0, reg5, 1'b0};
-    end
-  end
+  assign ip2reg_data = {reg3, 1'b0, reg4, 1'b0, reg5, 1'b0};
 
 endmodule
